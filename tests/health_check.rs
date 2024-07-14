11016f09aec6113ddff2d7dc1cea071aa4ee4c95
F. Joe Nichols <joe@fjoenichols.com>
2024-07-13 22:18:48 -0600

    changes from cargo fmt

--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,5 @@
+use sqlx::{Connection, PgConnection};
 use std::net::TcpListener;
-use sqlx::{PgConnection, Connection};
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
@@ -50,23 +50,23 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
     assert_eq!(200, response.status().as_u16());
 
     let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
-                    .fetch_one(&mut connection)
-                    .await
-                    .expect("Failed to fetch saved subscription");
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription");
 
     assert_eq!(saved.email, "ursula_le_guin@gmail.com");
     assert_eq!(saved.name, "le guin");
 }
 
 #[tokio::test]
-async  fn subscribe_returns_a_400_when_data_is_missing() {
+async fn subscribe_returns_a_400_when_data_is_missing() {
     let app_address = spawn_app();
     let client = reqwest::Client::new();
     let test_cases = vec![
         ("name=le%20guin", "missing the email"),
         ("email=ursula_le_guin%40gmail.com", "missing the name"),
-        ("", "missing both name and email")
-        ];
+        ("", "missing both name and email"),
+    ];
 
     for (invalid_body, error_message) in test_cases {
         let response = client
